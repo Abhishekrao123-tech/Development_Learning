c2ddb38a55f8217217d7840343c5b86b40251ade
Abhishek Rao <abhishekraoh410@gmail.com>
2026-08-31 19:52:43 +0530

    JS

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,31 @@
 let a = 10;
 let b = 20;
 
-console.log(a + b);
+// console.log(a + b);
 
-window.alert(10 + 66);
-document.write("HII...")
+// window.alert(10 + 66);
+// document.write("HII...");
+
+// for(initailze;condition;update){
+//     for loop body
+// }
+
+for (let i = 0; i < 5; i++) {
+  if (i % 2 == 0) {
+    // console.log(i);
+  }
+}
+
+let i = 0;
+do {
+  i++;
+//   console.log(i);
+} while (i < 5);
+let n = 0;
+let x = 1;
+while (n < 10) {
+  x += n;
+  console.log(x);
+  console.log(n);
+  n++;
+}
