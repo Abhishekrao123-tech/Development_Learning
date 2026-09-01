baa9139be0564786d1239fafa98c4abcd7c7a4b7
Abhishek Rao <abhishekraoh410@gmail.com>
2026-09-01 19:29:29 +0530

    JS

--- JS/script.js
+++ JS/script.js
@@ -16,16 +16,48 @@ for (let i = 0; i < 5; i++) {
   }
 }
 
-let i = 0;
-do {
-  i++;
+// let i = 0;
+// do {
+// i++;
 //   console.log(i);
-} while (i < 5);
-let n = 0;
-let x = 1;
-while (n < 10) {
-  x += n;
-  console.log(x);
-  console.log(n);
-  n++;
-}
+// } while (i < 5);
+// let n = 0;
+// let x = 1;
+// while (n < 10) {
+// x += n;
+// console.log(x);
+// console.log(n);
+// n++;
+// }
+
+// let x = 0;
+// let z = 0;
+
+// labelCancelledLoops: while (true) {
+//   console.log("Outer loop", x);
+//   x++;
+//   z = 1;
+//   while (true) {
+//     console.log("Inner loop", z);
+//     z++;
+//     if (z === 10 && x === 10) {
+//       break labelCancelledLoops;
+//     } else if (z === 10) {
+//       break;
+//     }
+//   }
+// }
+
+// break: exit loop
+// continue: skip the current iteration of the loop and continue with the next iteration
+
+// let i = 0;
+// let n = 0;
+// while (i < 5) {
+//   i++;
+//   if (i === 3) {
+//     continue;
+//   }
+//   n += i;
+//   console.log(n);
+// }
